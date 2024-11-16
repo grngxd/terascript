ef8679e8e805a288fccb1977d2d4f98a1a94d93f
grngxd <36968271+grngxd@users.noreply.github.com>
2024-11-16 22:39:43 +0000

    a

--- packages/cli/commands/build.ts
+++ packages/cli/commands/build.ts
@@ -4,32 +4,30 @@ import path from 'path';
 import { CommandModule } from 'yargs';
 
 export const buildCommand: CommandModule = {
-  command: 'build',
-  describe: 'Build the project',
-  builder: (yargs) => {
-    return yargs.option('prod', {
-      alias: 'p',
-      type: 'boolean',
-      description: 'Build for production',
-    });
-  },
-  handler: async (argv) => {
-    try {
-      const config = await getConfig();
+	command: 'build',
+	describe: 'Build the project',
+	builder: (yargs) => {
+		return yargs.option('prod', {
+		alias: 'p',
+		type: 'boolean',
+		description: 'Build for production',
+		});
+	},
+	handler: async (argv) => {
+		try {
+		const config = await getConfig();
 
-      console.log('Building project...');
+		console.log('Building project...');
 
-      const start = performance.now();
-
-      
-      await generator.generate().then((p) => {
-        const end = performance.now();
-        const duration = (end - start) / 1000;
-        console.log(`Project built to ${path.resolve(p)} in ${duration.toFixed(3)}s`);
-      });
-    } catch (e) {
-      console.error(e);
-      process.exit(1);
-    }
-  },
+		const start = performance.now();
+		await generator.generate().then((p) => {
+			const end = performance.now();
+			const duration = (end - start) / 1000;
+			console.log(`Project built to ${path.resolve(p)} in ${duration.toFixed(3)}s`);
+		});
+		} catch (e) {
+		console.error(e);
+		process.exit(1);
+		}
+	},
 };
